c2f22316ac665906cbd33c485f66d37a68df32ac
ScannableSplash <79610026+ScannableSplash@users.noreply.github.com>
2022-01-15 19:43:43 -0600

    Update Social Network (easy version).cpp

--- Clase Nivel 2/Lista de problemas para el semestre 2021-1/Social Network (easy version).cpp
+++ Clase Nivel 2/Lista de problemas para el semestre 2021-1/Social Network (easy version).cpp
@@ -1,1 +1,30 @@
-a
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k, m;
+    cin>>n>>k;
+    cout<<endl;
+    
+    vector<int> ID(n);
+    vector<int> v(k);
+    
+    for(int i=0; i<n; i++){
+        cin>>ID[i];
+        
+        for(int j=0; j<i; j++){
+            if(ID[i]=!v[j]){
+                v[i]=ID[i];
+            }
+        }
+        
+    }
+    
+    
+    
+    cout<<endl<<endl<<n<<endl<<k;
+
+    return 0;
+}
+
