b32146d877ea7fea4e201013ec9204855a402e11
ScannableSplash <79610026+ScannableSplash@users.noreply.github.com>
2022-01-20 15:56:29 -0600

    Create Learning Languages.cpp

---
+++ Clase Nivel 2/Lista de problemas para el semestre 2021-1/Learning Languages.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int n, m, k, x=0;
+    cin>>n>>m;
+    int matriz[n][m];
+    
+    for(int i=0; i<n; i++){
+        
+        cin>>matriz[i][0];
+        k=matriz[i][0];
+        
+        for(int j=1; j<k; j++){
+            cin>>matriz[i][j];
+        }
+        
+        cout<<endl;
+    }
+    
+    //*comparación perrona
+    
+    cout<<endl<<x;
+    
+    return 0;
+}
