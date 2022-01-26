9d5f38994f34dc8977012f464ae5b60a0d5cbf92
ScannableSplash <79610026+ScannableSplash@users.noreply.github.com>
2022-01-26 14:41:57 -0600

    Create Practica Ciclos ASINUS.cpp

---
+++ Nivel 1/listaProblemas_2021_2_Nivel1/Practica Ciclos ASINUS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int N,n,r=0;
+    cin>>N;
+    
+    for (int i=1; i<=N; i++){
+	    cin>>n;
+	    r=r+n;
+    }
+    
+    cout <<r;
+
+return 0;
+}
