8ef988278ba2389b78198b013e027da0948370d2
ScannableSplash <79610026+ScannableSplash@users.noreply.github.com>
2022-01-26 14:27:55 -0600

    Create Elephant.cpp

---
+++ Nivel 1/listaProblemas_2021_2_Nivel1/Elephant.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int A;
+    cin>>A;
+
+    if(A<=5) cout<<1;  
+    else{ 
+        if(A%5==0) cout<<A/5;
+        else cout<<(A/5)+1;
+    }
+
+return 0;
+}
