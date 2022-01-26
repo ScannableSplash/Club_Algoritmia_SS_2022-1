2deb951048f272681db7031ce9f8d6defca2c3b4
ScannableSplash <79610026+ScannableSplash@users.noreply.github.com>
2022-01-26 15:13:19 -0600

    Create Año Bisiesto.cpp

---
+++ Nivel 1/listaProblemas_2021_1_básicos/Año Bisiesto.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a;
+    cin>>a;
+
+    if(a%4==0||a%400==0) cout<<"BISIESTO";
+    else cout<<"NO BISIESTO";
+    
+return 0;
+}
