8127216f8e5f717e1d4a07468bcbc05b76c0bdfd
ScannableSplash <79610026+ScannableSplash@users.noreply.github.com>
2022-01-26 16:35:49 -0600

    Create Don "Tripas" ASINUS.cpp

---
+++ Nivel 1/listaProblemas_2021_1_básicos/Don "Tripas" ASINUS.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    cin >>N;
+    if (N>=1){
+        for(int i=0;i<N;i++){
+	        cin >>K;
+	    
+	        switch (K){
+	    	    case 1: 
+	    	    	r=r+3;
+	    	    	break;
+	    	    case 2: 
+	    	    	r=r+5;
+	    	    	break;
+	    	    case 3: 
+	    	    	r=r+4;
+	    	    	break;
+	    	    default:
+	    		    cout <<"INVALIDO"<<endl;
+	    		    return 0;
+	    	}
+		
+	    }
+	
+	    cout<<r;
+    }   
+    else cout<<"INVALIDO"<<endl;
+    
+return 0;
+}
