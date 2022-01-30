bc6efbd57909b10609a820394244876200749bf6
ScannableSplash <79610026+ScannableSplash@users.noreply.github.com>
2022-01-29 22:50:04 -0600

    Create Crucigrama.cpp

---
+++ Nivel 1/listaProblemas_2021_1_básicos/Crucigrama.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    string a, b;
+    cin>>a>>b;
+    
+    n=a.length();
+    m=b.length();
+    
+    //*Comparación perrona
+    
+    //*Impresión perrona
+
+return 0;
+}
