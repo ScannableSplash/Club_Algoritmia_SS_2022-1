294dee15c5f1c09fafd8424197e497a314cd8890
ScannableSplash <79610026+ScannableSplash@users.noreply.github.com>
2022-01-29 22:06:21 -0600

    Update Way Too Long Words.cpp

--- Nivel 1/listaProblemas_2021_1_básicos/Way Too Long Words.cpp
+++ Nivel 1/listaProblemas_2021_1_básicos/Way Too Long Words.cpp
@@ -14,24 +14,12 @@ int main(){
         size=s[i].length();
         string s2(to_string(size-2));
         
-        if(size>10){
-            r[i][0]=s[i][0];
-            
-            if(size-2<10){
-                r[i][1]=s2[0];
-                r[i][2]=s[i][size-1];
-            }
-            else{
-                r[i][1]=s2[0];
-                r[i][2]=s2[1];
-                r[i][3]=s[i][size-1];
-            }
-        }
+        if(size>10) r[i]=s[i][0]+s2+s[i][size-1];
         else r[i]=s[i];
     }
     
     for(int i=0; i<n; i++){
-        cout<<r[i];
+        cout<<r[i]<<endl;
     }
     
 return 0;
