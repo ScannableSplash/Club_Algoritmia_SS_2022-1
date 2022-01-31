45e3a1f5e73bcc629a0b4bddf8488b5987701fed
ScannableSplash <79610026+ScannableSplash@users.noreply.github.com>
2022-01-30 18:49:18 -0600

    Update Crucigrama.cpp

--- Nivel 1/listaProblemas_2021_1_básicos/Crucigrama.cpp
+++ Nivel 1/listaProblemas_2021_1_básicos/Crucigrama.cpp
@@ -3,15 +3,33 @@ using namespace std;
 
 int main(){
     
+    int x, y, z=0, n, m;
     string a, b;
     cin>>a>>b;
     
     n=a.length();
     m=b.length();
     
-    //*Comparación perrona
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(a[j]==b[i]){
+                x=j;
+                y=i;
+                break;
+            }
+        }
+    }
     
-    //*Impresión perrona
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(i==y) cout<<a[j];
+            else{
+                if(j==x) cout<<b[i];
+                else cout<<".";
+            }
+        }
+        cout<<endl;
+    }
 
 return 0;
 }
